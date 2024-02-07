e323804106112e63a514c5675860fa568d0ec663
Alireza <126352578+Alireza-Jodat2001@users.noreply.github.com>
2024-02-07 16:26:49 +0330

    The last video: 200. -D

--- script.js
+++ script.js
@@ -35,13 +35,18 @@ document.addEventListener(
 ///////////////////////////////////////
 // smooth scroll
 // 196
+// 200
+
+// for btnScrollTo
 const btnScroll = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 
 btnScroll.addEventListener('click', () => {
     // old way
+    // console.log(section1.getBoundingClientRect());
     // const { left: leftSec1, top: topSec1 } = section1.getBoundingClientRect();
     // const { pageXOffset, pageYOffset } = window;
+    // console.log(pageYOffset);
     // window.scrollTo({
     //     left: leftSec1 + pageXOffset,
     //     top: topSec1 + pageYOffset,
@@ -50,4 +55,37 @@ btnScroll.addEventListener('click', () => {
 
     // new way
     section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+// for navigation scroll
+// first way
+() => {
+    const links = document.querySelectorAll('.nav__link');
+
+    links.forEach(link =>
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+            document
+                .querySelector(this.getAttribute('href'))
+                .scrollIntoView({ behavior: 'smooth' });
+        })
+    );
+};
+
+// for navigation scroll
+// best way
+// use Delegation Implementing
+const ulNav = document.querySelector('.nav__links');
+
+ulNav.addEventListener('click', event => {
+    event.preventDefault();
+
+    const id =
+        !event.target.classList.contains('nav__link--btn') &&
+        event.target.getAttribute('href');
+
+    const section = document.querySelector(id);
+
+    event.target.classList.contains('nav__link') &&
+        section?.scrollIntoView({ behavior: 'smooth' });
 });
